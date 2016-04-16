844b5560ecfe3f0f819782ab1f4ed7c03186b1b5
Matias <mbascolo@gmail.com>
2016-04-16 11:42:25 -0300

    Ajustes

--- app/src/main/java/com/matias/tarjetasaludos/ApplicationConstants.java
+++ app/src/main/java/com/matias/tarjetasaludos/ApplicationConstants.java
@@ -3,7 +3,7 @@ package com.matias.tarjetasaludos;
 public interface ApplicationConstants {
 
 	// Php Application URL to store Reg ID created
-	static final String APP_SERVER_URL = "http://localhost/gcmwebapp/insertuser.php";
+	static final String APP_SERVER_URL = "http://www.intercap.com.ar/gcmwebapp/insertuser.php";
 
 	// Google Project Number
 	static final String GOOGLE_PROJ_ID = "872397807314";
